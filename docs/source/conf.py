9878795ad334020adc3f19357c18b7fefb450a68
Miyoung Choi <cmiyoung@amazon.com>
2021-09-29 18:02:21 -0700

    add adobe analytics

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -70,3 +70,8 @@ html_theme = "sphinx_rtd_theme"
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ["_static"]
+
+# For Adobe Analytics
+html_js_files = [
+    "https://a0.awsstatic.com/s_code/js/3.0/awshome_s_code.js",
+]
